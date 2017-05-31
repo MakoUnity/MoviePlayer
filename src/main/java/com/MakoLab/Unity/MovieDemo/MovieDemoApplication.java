1537e8daf3c3f135e790871062de6f7b322a8b06
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-05-31 10:45:32 +0200

    Add clear spring initializer project.

---
+++ src/main/java/com/MakoLab/Unity/MovieDemo/MovieDemoApplication.java
@@ -0,0 +1,12 @@
+package com.MakoLab.Unity.MovieDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDemoApplication.class, args);
+	}
+}
